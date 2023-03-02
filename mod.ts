4ae82af09d8d8187a54d3a8dbaea02bbfa8efb4d
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-02 12:33:33 +0900

    docs(mod): add code block

--- mod.ts
+++ mod.ts
@@ -4,8 +4,18 @@ import { withContentType } from "./content_length.ts";
 /** HTTP `content-length` header middleware.
  * Add or remove `content-length` headers in compliance with [RFC 9110, 8.6 Content-Length](https://www.rfc-editor.org/rfc/rfc9110.html#name-content-length) safely .
  *
- * @param request
- * @param next
+ * @example
+ * ```ts
+ * import contentLength from "https://deno.land/x/http_content_length@$VERSION/mod.ts";
+ * import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+ *
+ * declare const request: Request;
+ * const handler = () => new Response("hello");
+ *
+ * const response = await contentLength(request, handler);
+ *
+ * assertEquals(response.headers.get("content-length"), "5");
+ * ```
  */
 export default async function contentLength(
   request: Request,
