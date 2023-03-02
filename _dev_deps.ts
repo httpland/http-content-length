7f5cc4e3b657c792eb698c1775c522c154fba7dd
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-02 12:35:55 +0900

    test: add test case

--- _dev_deps.ts
+++ _dev_deps.ts
@@ -2,6 +2,7 @@ export { describe, it } from "https://deno.land/std@0.178.0/testing/bdd.ts";
 export {
   assert,
   assertEquals,
+  assertExists,
 } from "https://deno.land/std@0.178.0/testing/asserts.ts";
 export { equalsResponse } from "https://deno.land/x/http_utils@1.0.0-beta.7/responses.ts";
 export { Status } from "https://deno.land/std@0.178.0/http/http_status.ts";
---
+++ mod_test.ts
@@ -0,0 +1,16 @@
+import { assertEquals, assertExists, describe, it } from "./_dev_deps.ts";
+import contentLength from "./mod.ts";
+
+describe("default export", () => {
+  it("should exists", () => {
+    assertExists(contentLength);
+  });
+
+  it("should contain content-length header", async () => {
+    const handler = () => new Response("hello");
+
+    const response = await contentLength(new Request("test:"), handler);
+
+    assertEquals(response.headers.get("content-length"), "5");
+  });
+});
